c3262aaf12ae5213edad1c57cf20b9499e011adc
Ionut <ionut.rosan@nordlogic.com>
2021-06-05 13:52:04 +0300

    changes to port

--- server.js
+++ server.js
@@ -49,4 +49,6 @@ app.set("port", port);
 const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
-server.listen(port);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`)
+});
